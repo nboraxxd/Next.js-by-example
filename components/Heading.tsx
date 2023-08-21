f06c047b74292ea2ef4c8e5b1f58e4cbb94501e4
bora <nhatbaoxxd@gmail.com>
2023-08-21 12:36:43 +0700

    Font Optimization

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,7 +1,9 @@
+import { orbitron } from '@/app/font'
+
 interface HeadingProps {
   children: React.ReactNode
 }
 
 export default function Heading({ children }: HeadingProps) {
-  return <h1 className="font-bold text-2xl">{children}</h1>
+  return <h1 className={`font-bold text-2xl ${orbitron.className}`}>{children}</h1>
 }
