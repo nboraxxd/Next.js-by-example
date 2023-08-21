ea60b8fb9faab9da0ba07a9f8eedb8d60348af60
bora <nhatbaoxxd@gmail.com>
2023-08-21 12:26:00 +0700

    Image cards

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -5,12 +5,18 @@ export default function ReviewsPage() {
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul>
-        <li>
-          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+      <ul className="flex flex-col gap-3 mt-4">
+        <li className="group w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.0625rem] hover:mb-[0.0625rem]">
+          <Link href="/reviews/hollow-knight">
+            <img src="/images/hollow-knight.jpg" alt="Hollow Knight" width={320} height={180} className="rounded-t" />
+            <h2 className="py-2 text-center text-lg font-medium">Hollow Knight</h2>
+          </Link>
         </li>
-        <li>
-          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        <li className="w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.0625rem] hover:mb-[0.0625rem]">
+          <Link href="/reviews/stardew-valley">
+            <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={320} height={180} className="rounded-t" />
+            <h2 className="py-2 text-center text-lg font-medium">Stardew Valley</h2>
+          </Link>
         </li>
       </ul>
     </>
