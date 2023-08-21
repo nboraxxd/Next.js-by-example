95cff1d9ff4c15743822e371ecf0d0546cbdc9c2
bora <nhatbaoxxd@gmail.com>
2023-08-21 16:26:50 +0700

    Rendering Markdown

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,14 +1,17 @@
 import { readFile } from 'node:fs/promises'
+import { marked } from 'marked'
+
 import Heading from '@/components/Heading'
 
 export default async function StardewValley() {
   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
+  const html = marked(text)
 
   return (
     <>
       <Heading>Stardew Valley</Heading>
       <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={640} height={360} className="my-2 rounded" />
-      <p>{text}</p>
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </>
   )
 }
