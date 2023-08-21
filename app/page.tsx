95bbd0cdc151d3c2278d7a4c3443da303969495b
bora <nhatbaoxxd@gmail.com>
2023-08-21 16:11:55 +0700

    Reading md Files

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,21 @@ import Link from 'next/link'
 import Heading from '@/components/Heading'
 
 export default function HomePage() {
-  console.log('HomePage render')
-
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="my-3">Only the best indie games, reviewed for you</p>
 
       <div className="group w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.125rem] hover:mb-[0.125rem] md:w-full">
-        <Link href="/reviews/hollow-knight" className="flex flex-col md:flex-row">
+        <Link href="/reviews/stardew-valley" className="flex flex-col md:flex-row">
           <img
-            src="/images/hollow-knight.jpg"
+            src="/images/stardew-valley.jpg"
             alt="Hollow Knight"
             width={320}
             height={180}
             className="rounded-t md:rounded-l md:rounded-r-none"
           />
-          <h2 className="py-2 font-orbitron font-medium text-lg text-center md:grow">Hollow Knight</h2>
+          <h2 className="py-2 font-orbitron font-medium text-lg text-center md:grow">Stardew Valley</h2>
         </Link>
       </div>
     </>
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,11 +1,14 @@
+import { readFile } from 'node:fs/promises'
 import Heading from '@/components/Heading'
 
-export default function StardewValley() {
+export default async function StardewValley() {
+  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
+
   return (
     <>
       <Heading>Stardew Valley</Heading>
       <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={640} height={360} className="my-2 rounded" />
-      <p>This will be the review for Stardew Valley</p>
+      <p>{text}</p>
     </>
   )
 }
