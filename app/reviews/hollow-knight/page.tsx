3a8e672ee8b50cfc5278f2539a98cdbdba84e082
bora <nhatbaoxxd@gmail.com>
2023-08-21 11:13:52 +0700

    Static Assets

--- app/reviews/hollow-knight/page.tsx
+++ app/reviews/hollow-knight/page.tsx
@@ -4,6 +4,7 @@ export default function HollowKnight() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img src="/images/hollow-knight.jpg" alt="Hollow Knight" width={640} height={360} className='my-2 rounded' />
       <p>This will be the review for Hollow Knight</p>
     </>
   )
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -4,6 +4,7 @@ export default function StardewValley() {
   return (
     <>
       <Heading>Stardew Valley</Heading>
+      <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={640} height={360} className="my-2 rounded" />
       <p>This will be the review for Stardew Valley</p>
     </>
   )
