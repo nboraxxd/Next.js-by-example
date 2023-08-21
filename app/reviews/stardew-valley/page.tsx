d544e9417a2320935757d209126d5a2addca7c56
bora <nhatbaoxxd@gmail.com>
2023-08-21 17:00:59 +0700

    Front Matter

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,16 +1,21 @@
 import { readFile } from 'node:fs/promises'
 import { marked } from 'marked'
+import matter from 'gray-matter'
 
 import Heading from '@/components/Heading'
 
 export default async function StardewValley() {
   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
-  const html = marked(text)
-
+  const {
+    content,
+    data: { title, date, image },
+  } = matter(text)
+  const html = marked(content)
   return (
     <>
-      <Heading>Stardew Valley</Heading>
-      <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={640} height={360} className="my-2 rounded" />
+      <Heading>{title}</Heading>
+      <p className='italic my-2'>{date}</p>
+      <img src={image} alt="Stardew Valley" width={640} height={360} className="my-2 rounded" />
       <article className="max-w-screen-sm prose prose-slate" dangerouslySetInnerHTML={{ __html: html }} />
     </>
   )
