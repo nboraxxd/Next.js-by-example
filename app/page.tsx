58e80f73f6a2d9527025b17d18e938b49d61d56d
bora <nhatbaoxxd@gmail.com>
2023-08-22 19:12:10 +0700

    Exercise: Featured Review

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link'
 import Heading from '@/components/Heading'
+import { getFeaturedReview } from '@/lib/reviews'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const reviews = await getFeaturedReview()
+
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="my-3">Only the best indie games, reviewed for you</p>
 
       <div className="group w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.125rem] hover:mb-[0.125rem] md:w-full">
-        <Link href="/reviews/stardew-valley" className="flex flex-col md:flex-row">
+        <Link href={`/reviews/${reviews[0].slug}`} className="flex flex-col md:flex-row">
           <img
-            src="/images/stardew-valley.jpg"
-            alt="Hollow Knight"
+            src={reviews.image}
+            alt={reviews.title}
             width={320}
             height={180}
             className="rounded-t md:rounded-l md:rounded-r-none"
           />
-          <h2 className="py-2 font-orbitron font-medium text-lg text-center md:grow">Stardew Valley</h2>
+          <h2 className="py-2 font-orbitron font-medium text-lg text-center md:grow">{reviews.title}</h2>
         </Link>
       </div>
     </>
--- lib/reviews.ts
+++ lib/reviews.ts
@@ -2,6 +2,7 @@ import { readFile, readdir } from 'node:fs/promises'
 import { marked } from 'marked'
 import matter from 'gray-matter'
 import { v4 as uuidv4 } from 'uuid'
+import moment from 'moment'
 
 interface Review {
   id: string
@@ -27,11 +28,13 @@ export async function getReview(slug: string): Promise<Review> {
 export async function getReviews() {
   const slugs = await getSlugs()
   const reviews: Review[] = []
-  
+
   for (const slug of slugs) {
     const review = await getReview(slug)
     reviews.push(review)
   }
+
+  reviews.sort((a, b) => b.date.localeCompare(a.date))
 
   return reviews
 }
@@ -41,4 +44,9 @@ export async function getSlugs() {
   const slugs = files.filter((file) => file.endsWith('.md')).map((item) => item.slice(0, -'.md'.length))
 
   return slugs
+}
+
+export async function getFeaturedReview() {
+  const reviews = await getReviews()
+  return reviews[0]
 }
