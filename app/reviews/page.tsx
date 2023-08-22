e68c15e5722e9fc53c1a109134737176825699a4
bora <nhatbaoxxd@gmail.com>
2023-08-22 17:33:53 +0700

    Rendering Arrays

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,23 +1,26 @@
+import { Fragment } from 'react'
 import Link from 'next/link'
+import { getReviews } from '@/lib/reviews'
 import Heading from '@/components/Heading'
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const reviews = await getReviews()
+
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3 mt-4">
-        <li className="group w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.125rem] hover:mb-[0.125rem]">
-          <Link href="/reviews/hollow-knight">
-            <img src="/images/hollow-knight.jpg" alt="Hollow Knight" width={320} height={180} className="rounded-t" />
-            <h2 className="py-2 font-orbitron font-medium text-lg text-center">Hollow Knight</h2>
-          </Link>
-        </li>
-        <li className="w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.125rem] hover:mb-[0.125rem]">
-          <Link href="/reviews/stardew-valley">
-            <img src="/images/stardew-valley.jpg" alt="Stardew Valley" width={320} height={180} className="rounded-t" />
-            <h2 className="py-2 font-orbitron font-medium text-lg text-center">Stardew Valley</h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3 mt-4">
+        {reviews.map((review) => (
+          <li
+            key={review.id}
+            className="w-80 border rounded bg-white shadow transition-all hover:shadow-lg hover:-mt-[0.125rem] hover:mb-[0.125rem]"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img src={review.image} alt={review.title} width={320} height={180} className="rounded-t" />
+              <h2 className="py-2 font-orbitron font-medium text-lg text-center">{review.title}</h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   )
