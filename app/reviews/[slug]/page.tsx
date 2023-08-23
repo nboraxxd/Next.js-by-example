215a699ec47f0e3f82755e8ab250085e583d205d
bora <nhatbaoxxd@gmail.com>
2023-08-24 04:55:30 +0700

    Dynamic Metadata

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,17 +1,30 @@
 import { getReview, getSlugs } from '@/lib/reviews'
 import Heading from '@/components/Heading'
+import { Metadata } from 'next'
 
-interface Props {
-  params: { slug: string }
+interface ReviewPageParams {
+  slug: string
 }
 
-export async function generateStaticParams() {
+interface ReviewPageProps {
+  params: ReviewPageParams
+}
+
+export async function generateStaticParams(): Promise<ReviewPageParams[]> {
   const slugs = await getSlugs()
 
   return slugs.map((slug) => ({ slug }))
 }
 
-export default async function ReviewPage({ params: { slug } }: Props) {
+export async function generateMetadata({ params: { slug } }: ReviewPageProps): Promise<Metadata> {
+  const review = await getReview(slug)
+
+  return {
+    title: review.title,
+  }
+}
+
+export default async function ReviewPage({ params: { slug } }: ReviewPageProps) {
   const review = await getReview(slug)
 
   return (
