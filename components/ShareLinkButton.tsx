4a273e73442814d444a24651c6343a51b8ae96ad
bora <nhatbaoxxd@gmail.com>
2023-08-24 09:16:49 +0700

    Client Components & hydration process

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+'use client'
 
 export default function ShareLinkButton() {
   function onClickButton() {
-    console.log('hihi')
+    console.log('clicked')
   }
+  console.log('render')
   return (
     <button
       className="px-2 py-1 border rounded text-slate-500 font-medium text-sm transition-all hover:bg-orange-100 hover:text-slate-700"
