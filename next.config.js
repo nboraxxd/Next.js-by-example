64d51cc29abb7ed8f8b8c2265e0e0d4d847f3dae
bora <nhatbaoxxd@gmail.com>
2023-08-29 14:50:27 +0700

    ernative Image Loaders

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // output: 'export',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '1337',
-          pathname: '/uploads/**'
-        }
-      ]
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**'
+      }
+    ]
+  },
 }
